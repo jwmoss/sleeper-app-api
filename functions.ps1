8d27c5a2eb97954d98dd34e8e3f894b65a6736b6
Jonathan Moss <jwmoss88@gmail.com>
2021-09-09 17:10:10 -0400

    Updates

--- functions.ps1
+++ functions.ps1
@@ -10,7 +10,11 @@ function Get-SLLeagueUsers ($LeagueID) {
     Invoke-RestMethod -Uri "https://api.sleeper.app/v1/league/$leagueID/users"
 }
 
-function Get-SLLeagueMatchups ($LeagueID,$Week) {
+function Get-SLLeagueUser ($UserID) {
+    Invoke-RestMethod -Uri "https://api.sleeper.app/v1/user/$userid"
+} 
+
+function Get-SLLeagueMatchups ($LeagueID, $Week) {
     Invoke-RestMethod -Uri "https://api.sleeper.app/v1/league/$leagueID/matchups/$week"
 }
 
@@ -20,4 +24,44 @@ function Get-SLNFLState {
 
 function Get-SLPlayers {
     Invoke-RestMethod -Uri "https://api.sleeper.app/v1/players/nfl"
+}
+
+function Get-SLScoreBoard ($LeagueID) {
+
+}
+
+function Get-SLHighScore ($LeagueID) {
+
+}
+
+function Get-SLLowScore ($LeagueID) {
+
+}
+
+function Get-SLTrending {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [Hashtable]
+        $playerLookup
+    )
+
+    $add = Invoke-RestMethod -Uri "https://api.sleeper.app/v1/players/nfl/trending/add?lookback_hours=48"
+    $drop = Invoke-RestMethod -Uri "https://api.sleeper.app/v1/players/nfl/trending/drop?lookback_hours=48"
+    foreach ($u in $add) {
+        [PSCustomObject]@{
+            ID = $u.player_id
+            Name = $playerLookup[$u.player_id].full_name
+            Count = $u.count 
+            Type = "Add"
+        }
+    }
+    foreach ($u in $drop) {
+        [PSCustomObject]@{
+            ID = $u.player_id
+            Name = $playerLookup[$u.player_id].full_name
+            Count = $u.count 
+            Type = "Drop"
+        }
+    }
 }
